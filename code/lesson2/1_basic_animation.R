59360f938732ecfe5fda4dd37f44e4635bc497d1
Bruce Delo <bruce@bjones-dongle-4.ocean.dal.ca>
2023-10-10 12:29:49 -0300

    Made animation lesson standalone. Room remains for additional tweaks to which specific data we use, but as it stands, the code will run without needing to run any other blocks.

--- code/lesson2/1_basic_animation.R
+++ code/lesson2/1_basic_animation.R
@@ -7,10 +7,10 @@ library(ggmap)
 library(tidyverse)
 
 
-setwd("YOUR/PATH/TO/data/act")
+setwd("/Users/bruce/Work/2023-canssi-ecr-workshop/data/otn")
 
 detection_events <- #create detections event variable
-  read_otn_detections('proj58_matched_detections_2016.csv') %>% # reading detections
+  read_otn_detections('nsbs_matched_detections_2022/nsbs_matched_detections_2022.csv') %>%
   false_detections(tf = 3600) %>%  #find false detections
   filter(passed_filter != FALSE) %>% 
   detection_events(location_col = 'station', time_sep=3600)
@@ -18,7 +18,7 @@ detection_events <- #create detections event variable
 plot_data <- detection_events %>% 
   dplyr::select(animal_id, mean_longitude,mean_latitude, first_detection)
 
-one_fish <- plot_data[plot_data$animal_id == "PROJ58-1218508-2015-10-13",] 
+one_fish <- plot_data[plot_data$animal_id == "NSBS-1393342-2021-08-10",] 
 
 basemap <- 
   get_stamenmap(
@@ -28,21 +28,21 @@ basemap <-
              top = max(one_fish$mean_latitude)),
     maptype = "toner-lite",
     crop = FALSE, 
-    zoom = 8)
+    zoom = 7)
 
-act.plot <-
+otn.plot <-
   ggmap(basemap) +
   geom_point(data = one_fish, aes(x = mean_longitude, y = mean_latitude), size = 2) +
   geom_path(data = one_fish, aes(x = mean_longitude, y = mean_latitude)) +
   labs(title = "ACT animation",
        x = "Longitude", y = "Latitude", color = "Tag ID")
 
-ggplotly(act.plot)
+ggplotly(otn.plot)
 
-act.plot <-
-  act.plot +
+otn.plot <-
+  otn.plot +
   labs(subtitle = 'Date: {format(frame_along, "%d %b %Y")}') +
   transition_reveal(first_detection) +
   shadow_mark(past = TRUE, future = FALSE)
 
-animate(act.plot)
+animate(otn.plot)
