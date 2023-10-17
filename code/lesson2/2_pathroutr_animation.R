426966b6fc07d7f8caeca48da8ba9e842018d11a
Bruce Delo <bruce@bjones-dongle-4.ocean.dal.ca>
2023-10-17 11:17:59 -0300

    Tested and tweaked pathroutr lesson to make it work on the NSBS data and also run in a more reasonable timeframe.

--- code/lesson2/2_pathroutr_animation.R
+++ code/lesson2/2_pathroutr_animation.R
@@ -6,11 +6,12 @@ library(pathroutr)
 library(ggspatial)
 library(sp)
 library(raster)
+library(geodata)
 
-setwd("YOUR/PATH/TO/data/act")
+setwd("YOUR/PATH/TO/data/otn")
 
 detection_events <- #create detections event variable
-  read_otn_detections('proj58_matched_detections_2016.csv') %>% # reading detections
+  read_otn_detections('nsbs_matched_detections_2022.csv') %>% # reading detections
   false_detections(tf = 3600) %>%  #find false detections
   filter(passed_filter != FALSE) %>% 
   detection_events(location_col = 'station', time_sep=3600)
@@ -18,16 +19,16 @@ detection_events <- #create detections event variable
 plot_data <- detection_events %>% 
   dplyr::select(animal_id, mean_longitude,mean_latitude, first_detection)
 
-one_fish <- plot_data[plot_data$animal_id == "PROJ58-1218518-2015-09-16",]
+one_fish <- plot_data[plot_data$animal_id == "NSBS-1393342-2021-08-10",] 
 
-one_fish <- one_fish %>% filter(mean_latitude < 38.90 & mean_latitude > 38.87) %>% 
-  slice(155:160)
+#one_fish <- one_fish %>% filter(mean_latitude < 38.90 & mean_latitude > 38.87) %>% 
+#  slice(155:160)
 
-USA<-getData('GADM', country='USA', level=1)
+CAN<-gadm('CANADA', level=1, path="./geodata", resolution=2)
 
-shape_file <- USA[USA$NAME_1 == 'Maryland',]
+shape_file <- CAN[CAN$NAME_1 == 'Nova Scotia',]
 
-md_polygon <- st_as_sf(shape_file)  %>% st_transform(5070)
+ns_polygon <- st_as_sf(shape_file)  %>% st_transform(5070)
 
 path <- one_fish %>%  dplyr::select(mean_longitude,mean_latitude)
 
@@ -36,7 +37,7 @@ path <- SpatialPoints(path, proj4string = CRS("+proj=longlat +datum=WGS84 +no_de
 path <-  st_as_sf(path)  %>% st_transform(5070)
 
 ggplot() + 
-  ggspatial::annotation_spatial(md_polygon, fill = "cornsilk3", size = 0) +
+  ggspatial::annotation_spatial(ns_polygon, fill = "cornsilk3", size = 2) +
   geom_point(data = path, aes(x=unlist(map(geometry,1)), y=unlist(map(geometry,2)))) +
   geom_path(data = path, aes(x=unlist(map(geometry,1)), y=unlist(map(geometry,2))))  +
   theme_void()
@@ -45,14 +46,16 @@ plot_path <- path %>% st_cast('MULTIPOINT') %>% summarise(do_union = FALSE) %>% 
 
 track_pts <- st_sample(plot_path, size = 10000, type = "regular")
 
-vis_graph <- prt_visgraph(md_polygon, buffer = 150)
+#This takes a WHILE to run. 
+vis_graph <- prt_visgraph(ns_polygon, buffer = 100)
 
-track_pts_fix <- prt_reroute(track_pts, md_polygon, vis_graph, blend = TRUE)
+#This too.
+track_pts_fix <- prt_reroute(track_pts, ns_polygon, vis_graph, blend = TRUE)
 
 track_pts_fix <- prt_update_points(track_pts_fix, track_pts)
 
 pathroutrplot <- ggplot() + 
-  ggspatial::annotation_spatial(md_polygon, fill = "cornsilk3", size = 0) +
+  ggspatial::annotation_spatial(ns_polygon, fill = "cornsilk3", size = 0) +
   geom_point(data = track_pts_fix, aes(x=unlist(map(geometry,1)), y=unlist(map(geometry,2)))) +
   geom_path(data = track_pts_fix, aes(x=unlist(map(geometry,1)), y=unlist(map(geometry,2))))  +
   theme_void()
@@ -64,4 +67,4 @@ pathroutrplot.animation <-
   transition_reveal(fid) +
   shadow_mark(past = T, future = F)
 
-gganimate::animate(pathroutrplot.animation, nframes=200)
+gganimate::animate(pathroutrplot.animation, nframes=100)
